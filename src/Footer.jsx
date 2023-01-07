b71ff8e37c2412bca25dc1703367022c0d81c362
DavoDev <davidlarrosa58@gmail.com>
2023-01-07 20:23:10 +0100

    Prueba de commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -15,7 +15,7 @@ export function Footer() {
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#work">Work</a></li>
-        <li><a href="#projects">Projects!!!</a></li>
+        <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
         </ul>
         <p>Copyright <AiOutlineCopyright></AiOutlineCopyright> 2022 davodeveloper.com</p>
