2647da7a3d8639d2cf95bf7ab07432f1810c4202
DavoDev <davidlarrosa58@gmail.com>
2023-02-15 18:55:45 +0100

    ajuste work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -18,7 +18,7 @@ const Work = () => {
                     </div>
                     <div className='work'>
                         <h3>La Pera Limonera</h3>
-                        <p>React FrontEnd Developer <span>- June 2021 to Present -</span></p>
+                        <p>React FrontEnd Developer <span>- June 2022 to Present -</span></p>
                         <p>Developments with React JS modern features:<br></br> hooks, life-cycle methods; managing status and APIs; testing with Jest-Enzyme.</p>
                     </div>
                     <div className='square'>
@@ -29,7 +29,7 @@ const Work = () => {
                     </div>
                     <div className='work'>
                         <h3>Freelance Developer</h3>
-                        <p>Full Stack Web Developer <span>- February 2020 to June 2021 -</span></p>
+                        <p>Full Stack Web Developer <span>- February 2021 to June 2022 -</span></p>
                         <p>Web Project Manager for MyGreenPepper & Fotofiguras Mascotas: Design, development, deployment, updating and maintenance.</p>
                     </div>
                     <div className='square'>
@@ -40,8 +40,8 @@ const Work = () => {
                     </div>
                     <div className='work'>
                         <h3>Fotofiguras Mascotas</h3>
-                        <p>Autónomo <span>- April 2015 to September 2020 -</span></p>
-                        <p>Full stack web development and deploy, customers support, production, shipment, sales, financial control..</p>
+                        <p>Autónomo <span>- April 2015 to September 2021 -</span></p>
+                        <p>Web development and deploy, customers support, production, shipment, sales, financial control..</p>
                     </div>
                     <div className='square'>
                     </div>
